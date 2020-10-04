c934917d722fd12ae102452c7d61b2872d2ebc33
Atri Das <babinsamrat123@gmail.com>
2020-10-04 11:37:52 +0530

    refreshes the code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Intent operation
+# Day_with_Intent
+A brief about Intent
--- app/src/main/java/com/example/backup/details.java
+++
@@ -1,6 +0,0 @@
-package com.example.backup;
-
-import android.app.Activity;
-
-public class details extends Activity {
-}
